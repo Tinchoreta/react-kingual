96a2d62cf0cb7832d04071c8bcf0c7aef8e18dc0
Martin Reta <tinchoreta@gmail.com>
2023-02-13 23:35:51 -0300

    Adding carrito fontawesome

---
+++ src/componentes/CartWidget/CartWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+const CartWidget = () => {
+  return (
+    <FontAwesomeIcon icon="fa-regular fa-cart-shopping" />
+  )
+}
+
+export default CartWidget
--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import NavBar from './componentes/NavBar';
+import App from './App';
 
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar />
+    <App />
   </React.StrictMode>
 );
 
