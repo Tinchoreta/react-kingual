ebc4575704ec64cf87099b5a40e3207230f721d0
Martin Reta <tinchoreta@gmail.com>
2023-02-19 19:22:29 -0300

    Agrego Item, modifico BBDD Json, y agrego ItemList

---
+++ src/componentes/ItemList/ItemList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const cursoImage = require.context('../../assets/img/',true)
+const ItemList = (props) => {
+  return (
+    <div>ItemList</div>
+          )
+}
+
+export default ItemList
