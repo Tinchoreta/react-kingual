1c5d67a91d97aacfa530f5a34b7e804c398bb58b
Martin Reta <tinchoreta@gmail.com>
2023-02-20 19:10:04 -0300

    Agrego ItemListContainer con pedido a BBDD fetch - useState y useEffect

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const ItemListContainer = (props) => {
+const cursoImage = require.context('../../assets/img/',true)
+const URL= "../../bbdd/bbdd.json"
+
+const ItemListContainer = ({greeting}) => {
+
+  
+  const [listaCursos, setListaCursos] = useState([])
+
+  // Usamos un efecto para cargar los datos del Json de cursos al montar el componente.
+
+  useEffect(() => {
+    fetch(URL)
+    .then(response => response.json())
+    .then(dataJson => setListaCursos(dataJson.results))
+  }, [])
+
   return (
-    <p>{props.greeting}</p>
+    <>
+      <p>{greeting}</p>
+      {
+        listaCursos.map(curso =>(
+          <div className="card ocultar">
+              <img src={cursoImage(`${curso.imgUrl}`)} alt={curso.nombre} />
+              <div>
+                  <p>{curso.nombre}</p>
+                  <p className="label-curso">{curso.nivel}</p>
+              </div>
+              <div>
+                  <button className="card-button">AGREGAR</button>
+              </div>
+          </div>
+        )
+        )
+      }
+    </>
   )
 }
 
