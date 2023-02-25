75a036976ae295279f681f54af752b4db0e60f0b
Martin Reta <tinchoreta@gmail.com>
2023-02-25 15:38:42 -0300

    Add Routes on App

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import React from 'react';
 import NavBar from './componentes/NavBar/NavBar';
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
-import ItemDetailsContainer from './componentes/ItemDetailsContainer';
-import Cart from './componentes/Cart';
+import ItemDetailsContainer from './componentes/ItemDetailsContainer/ItemDetailsContainer';
+import Cart from './componentes/Cart/Cart';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
