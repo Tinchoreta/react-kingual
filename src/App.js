82bf13cdd4872578a523629f016e20510d523751
Martin Reta <tinchoreta@gmail.com>
2023-02-25 16:09:11 -0300

    Add NavLink on NavBar y route para cada ruta - import fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import './App.css';
 import React from 'react';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import NavBar from './componentes/NavBar/NavBar';
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
 import ItemDetailsContainer from './componentes/ItemDetailsContainer/ItemDetailsContainer';
 import Cart from './componentes/Cart/Cart';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
 
   return (
-    <BrowserRouter>
 
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<ItemListContainer greeting='Hola, bienvenido al carrito de compras de Kingual Education®.' />} />
-        <Route path='/categoria/:categoriaId' element={<ItemListContainer greeting='Categorias de cursos' />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/detalle' element={<ItemDetailsContainer />} />
-      </Routes>
+    <>
+      <BrowserRouter>
 
-    </BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<ItemListContainer greeting='Hola, bienvenido al carrito de compras de Kingual Education®.' />} />
+          <Route path='/categoria/:categoriaId' element={<ItemListContainer greeting='Categorias de cursos' />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/detalle' element={<ItemDetailsContainer />} />
+        </Routes>
 
+      </BrowserRouter>
+    </>
   );
 }
 
