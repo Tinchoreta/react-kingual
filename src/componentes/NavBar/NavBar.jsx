73c1d27a621633f488e5cabca0d55c273078e592
Martin Reta <tinchoreta@gmail.com>
2023-02-25 16:30:25 -0300

    Errores en navlinks navbar

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -6,61 +6,54 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <NavLink className="navbar-brand ms-4" to='/'/>
+      <NavLink className="navbar-brand ms-4" to='/'>
         Kingual®
-      
+      </NavLink>
       <button
         className="navbar-toggler"
         type="button"
         data-bs-toggle="collapse"
         data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
+        aria-controls="navbarSupportedContent"        
         aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+        aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <NavLink className="nav-link" to='/'/>
+            <NavLink className="nav-link" to='/'>
               Principal <span className="sr-only">(current)</span>
-            
+            </NavLink>
           </li>
 
           <li className="nav-item dropdown">
             <NavLink
               className="nav-link dropdown-toggle"
-              to=''
+              to='/'
               id="navbarDropdown"
               role="button"
               data-bs-toggle="dropdown"
               aria-haspopup="true"
               aria-expanded="false"
-            />
+            >
               Cursos
-            
+            </NavLink>
             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <NavLink className="dropdown-item" to='/categoria/english'/>
-                👑 Inglés
-              
-              <NavLink className="dropdown-item" to='/categoria/chinese'/>
-                🎎 Chino
-              
-              <NavLink className="dropdown-item" to='/categoria/portuguese'/>
-                {" "}
+            
+              <NavLink className="dropdown-item" to='/categoria/english'>👑 Inglés</NavLink>
+              <NavLink className="dropdown-item" to='/categoria/chinese'>🎎 Chino </NavLink>
+              <NavLink className="dropdown-item" to='/categoria/portuguese'>
                 🥑 Portugés
-              
-              <NavLink className="dropdown-item" to='/categoria/russian'/>
-                {" "}
+              </NavLink>
+              <NavLink className="dropdown-item" to='/categoria/russian'>
                 ⛄ Ruso
-              
+              </NavLink>
               <div className="dropdown-divider"></div>
-              <NavLink className="dropdown-item" to='/categoria/spanish'/>
-                {" "}
+              <NavLink className="dropdown-item" to='/categoria/spanish'>
                 🍺 Español para extranjeros
-              
+              </NavLink>
             </div>
           </li>
           <li className="nav-item">
