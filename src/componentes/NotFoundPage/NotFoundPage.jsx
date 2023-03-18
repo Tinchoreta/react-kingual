3a2e3c159c83722c00ecafbdaf0a1c862e692063
Martin Reta <tinchoreta@gmail.com>
2023-03-18 18:17:22 -0300

    NotFoundPage css

--- src/componentes/NotFoundPage/NotFoundPage.jsx
+++ src/componentes/NotFoundPage/NotFoundPage.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import { hydrate } from 'react-dom';
 import { Link } from 'react-router-dom';
+import './NotFoundPage.css';
+
 
 const pathImg = require.context('../../assets/img/', true)
 
 // Componente principal de la página 404
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404 Error: No se encontró la página</h1>
-      <Link to={`/`}>
-        <img src={pathImg(`./404.jpg`)} alt="Imagen de error 404" />
-      </Link>
-      {/* <p>Lo sentimos, parece que la página que estás buscando no existe o ha sido eliminada. Pero no te preocupes, te invitamos a explorar nuestro sitio web para encontrar más recursos y herramientas para mejorar tus habilidades en idiomas.</p>
-      <a>Haz clic aquí para explorar nuestras opciones de cursos y recursos gratuitos</a> */}
-    </div>
+    // <div>
+    //   <h1>404 Error: No se encontró la página</h1>
+    //     <Link to="/" target="_self">
+    //       <img src={pathImg(`./404.jpg`)} alt="Imagen de error 404" />
+    //     </Link>
+
+    //     <p>Lo sentimos, parece que la página que estás buscando no existe o ha sido eliminada. Pero no te preocupes, te invitamos a explorar nuestro sitio web para encontrar más recursos y herramientas para mejorar tus habilidades en idiomas.</p>
+    //   <a href="/">Haz clic aquí para explorar nuestras opciones de cursos y recursos gratuitos</a>
+    
+    // </div>
+
+
+    <div className="error-container">
+    <h1 className="error-title">404 Error: No se encontró la página</h1>
+    <Link to="/" target="_self">
+    
+           <img src={pathImg(`./404.jpg`)} alt="Imagen de error 404" />
+    
+    </Link>
+    <p className="error-text">Lo sentimos, parece que la página que estás buscando no existe o ha sido eliminada. Pero no te preocupes, te invitamos a explorar nuestro sitio web para encontrar más recursos y herramientas para mejorar tus habilidades en idiomas.</p>
+    <Link className="error-link" to="/">Haz clic aquí para explorar nuestras opciones de cursos y recursos gratuitos</Link>
+  </div>
   );
 };
 
-// Renderizar la página 404 en el DOM
-if (typeof window !== 'undefined') {
-  hydrate(<NotFoundPage />, document.getElementById('root'));
-}
+
 
 export default NotFoundPage
