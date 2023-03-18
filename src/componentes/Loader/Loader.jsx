894b14c203686101a7a6b33ebbde133983f96656
Martin Reta <tinchoreta@gmail.com>
2023-03-18 18:28:01 -0300

    Loader page

---
+++ src/componentes/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Loader.css'
+
+
+const Loader = () => {
+    return (
+
+        <div class="loader">
+            <div class="spinner"></div>
+            <div class="message">Cargando...</div>
+        </div>
+      )
+}
+
+export default Loader
