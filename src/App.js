02308524f7ec7fd8079a19fd4e9565ec9bfb6779
Martin Reta <tinchoreta@gmail.com>
2023-03-22 02:36:27 -0300

    Firebase funcionando

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import ItemBase from './componentes/ItemBase/ItemBase';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
