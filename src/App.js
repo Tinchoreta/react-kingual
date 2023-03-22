9fc746b57b2d3abd3d53e4c158bcf9ddcaa9b612
Martin Reta <tinchoreta@gmail.com>
2023-03-22 02:08:30 -0300

    agrego const de firebase en app

--- src/App.js
+++ src/App.js
@@ -12,6 +12,15 @@ import CartProvider from './context/CartContext';
 import Footer from './componentes/Footer/Footer';
  
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDim_mG0_vC8FwSzhygRVC7eucwCx4TLIk",
+  authDomain: "kingual.firebaseapp.com",
+  projectId: "kingual",
+  storageBucket: "kingual.appspot.com",
+  messagingSenderId: "725176992363",
+  appId: "1:725176992363:web:a002688b874422cf2da72e",
+  measurementId: "G-C11LG3CYXB"
+};
 
 
 function App() {
