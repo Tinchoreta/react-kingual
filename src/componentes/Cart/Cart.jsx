cc78cba4942006b48ae61a652d411827e8bae3fa
Martin Reta <tinchoreta@gmail.com>
2023-03-24 22:38:24 -0300

    Cambios en presentación datos en modal de Cart

--- src/componentes/Cart/Cart.jsx
+++ src/componentes/Cart/Cart.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../../context/CartContext';
@@ -7,15 +6,13 @@ import './Cart.css';
 
 function Cart() {
   const { cartItems, removeProduct, clearCart, calculateTotalQuantity,
-      calculateTotalCartPrice } = useCartContext();
+    calculateTotalCartPrice } = useCartContext();
 
-const total = calculateTotalCartPrice();
+  const total = calculateTotalCartPrice();
 
-const totalCartItems = calculateTotalQuantity();
+  const totalCartItems = calculateTotalQuantity();
 
   // Manejo de la ventana modal de checkout 
-
-
   const [showModal, setShowModal] = useState(false);
 
   const handleShowModal = () => {
@@ -30,24 +27,28 @@ const totalCartItems = calculateTotalQuantity();
     <div className="cart">
       <div className="cart-header">
         <h2>Carrito de Compras</h2>
-        <span className="cart-total">{`Total: $${parseFloat(total).toFixed(2)}`}</span>
       </div>
-      {cartItems.length > 0 ? cartItems.map((curso) => (
-        <div className="cart-item" key={curso.item.id}>
-          <span className="cart-item-name">{curso.item.nombre}</span>
-          <span className="cart-item-price"> {curso.item.precio === undefined ? '' : `$${(+curso.item.precio).toFixed(2)}`}</span>
-          <span className="cart-item-quantity"> {curso.quantity === undefined ? '' : `Unidades: ${(+curso.quantity)}`}</span>
-          <button className="cart-item-remove" onClick={() => removeProduct(curso.item.id)}>
-            Quitar
-          </button>
-
-        </div>
-      )) : <div>No hay items</div>
-      }
-      <div className='cart-total-courses'>
-        <span className="cart-total">{`Total de cursos: ${totalCartItems}`}</span>
-      </div>
-
+      {cartItems.length > 0 ? (
+        <>
+          {cartItems.map((curso) => (
+            <div className="cart-item" key={curso.item.id}>
+              <span className="cart-item-name">{curso.item.nombre}</span>
+              <span className="cart-item-quantity"> {curso.quantity === undefined ? '' : `Unidades: ${(+curso.quantity)}`}</span>
+              <span className="cart-item-price"> {curso.item.precio === undefined ? '' : `Precio unitario: $ ${(+curso.item.precio).toFixed(2)}`}</span>
+              <span className="cart-item-subtotal"> {curso.item.precio === undefined ? '' : `Subtotal: $ ${(+curso.item.precio * curso.quantity).toFixed(2)}`}</span>
+              <button className="cart-item-remove" onClick={() => removeProduct(curso.item.id)}>
+                Quitar
+              </button>
+            </div>
+          ))}
+          <div className='cart-total-courses'>
+            <span className="cart-total">{`Total de cursos: ${totalCartItems}`}</span>
+            <span className="cart-total-price">{`Total: $${parseFloat(total).toFixed(2)}`}</span>
+          </div>
+        </>
+      ) : (
+        <div className='no-items-to-show'>Carrito vacío</div>
+      )}
       <div className="cart-buttons">
         <button className="cart-button cart-button-clear" onClick={clearCart}>
           Vaciar carrito
@@ -58,46 +59,44 @@ const totalCartItems = calculateTotalQuantity();
         <button className="cart-button cart-button-buy" onClick={handleShowModal}>
           Comprar cursos
         </button>
-
-
-
       </div>
 
-      {/* Checkout abre ventana modal  */}
 
-      <Modal isOpen={showModal} onRequestClose={handleCloseModal}>
-        <div className="modal">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h3 className="modal-title">Resumen de Compra</h3>
-              <span className="modal-close" onClick={handleCloseModal}>
-                X
-              </span>
-            </div>
-            <div className="modal-body">
-              {cartItems.map((curso) => (
-                <div className="modal-item" key={curso.item.id}>
-                  <span className="modal-item-name">{curso.item.nombre}</span>
-                  <span className="modal-item-price">{`$${(+curso.item.precio).toFixed(2)}`}</span>
-                  <span className="modal-item-quantity">{curso.quantity}</span>
-                </div>
-              ))}
-            </div>
-            <div className="modal-total">
-              <span className="modal-total-label">Total:</span>
-              <span className="modal-total-amount">{`$${total.toFixed(2)}`}</span>
-            </div>
-            <div className="modal-buttons">
-              <button className="modal-button-close" onClick={handleCloseModal}>
-                Cerrar
-              </button>
-              
-            </div>
+{/* Checkout abre ventana modal  */ }
+
+<Modal isOpen={showModal} onRequestClose={handleCloseModal}>
+  <div className="modal">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h3 className="modal-title">Resumen de Compra</h3>
+        <span className="modal-close" onClick={handleCloseModal}>
+        </span>
+      </div>
+      <div className="modal-body">
+        {cartItems.map((curso) => (
+          <div className="modal-item" key={curso.item.id}>
+            <span className="modal-item-name">{curso.item.nombre}</span>
+            <span className="modal-item-price">{`$${(+curso.item.precio).toFixed(2)}`}</span>
+            <span className="modal-item-quantity">cantidad: {curso.quantity}</span>
+            <span className="modal-item-subtotal"> {curso.item.precio === undefined ? '' : `Subtotal: $ ${(+curso.item.precio * curso.quantity).toFixed(2)}`}</span>
           </div>
-        </div>
-      </Modal>
+        ))}
+      </div>
+      <div className="modal-total">
+        <span className="modal-total-label">Total:</span>
+        <span className="modal-total-amount">{`$${total.toFixed(2)}`}</span>
+      </div>
+      <div className="modal-buttons">
+        <button className="modal-button-close" onClick={handleCloseModal}>
+          Cerrar
+        </button>
 
+      </div>
     </div>
+  </div>
+</Modal>
+
+    </div >
   );
 }
 
